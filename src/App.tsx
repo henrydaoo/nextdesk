755a7822919175f23a39f2ef93261d21bf6e765c
Duy Thong <duythong28112004@gmail.com>
2025-07-27 12:26:07 +0700

    feat: add service section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
+import ServicesSection from "./components/ServicesSection";
 
 const App = () => {
   return (
     <div className="min-h-screen">
       <Header />
       <HeroSection />
+      <ServicesSection />
     </div>
   );
 };
